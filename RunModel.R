ff1d98410bf9eb21f804e19e9143e96b0fb986ab
jordahn <63667525+jordahn@users.noreply.github.com>
2021-04-16 10:55:30 -0400

    Update RunModel.R
    Hi Dr. Willoughby,
    
    I only slightly changed your template when playing around with the model to add a few column names and an output table. I also had to change "nrow(intransit)" to "length(intransit)" because it was a vector. 
    
    Ultimately, I think it is best that the starting point be assuming some population of mosquitos are on board the ship such as defined in this model as "wildtocargo." 
    
    I am also not very familiar with GitHub so let me know if there are other questions or changes I can answer.
    
    Thank you!
    Jordan

--- RunModel.R
+++ RunModel.R
@@ -5,33 +5,40 @@ setwd("~/GDrive/Willoughby lab/transport risk/")
 wildtocargo  = c(10)
 survivetrans = 0.9
 exitcargo    = 0.5
-newenv       = 1
+newenv       = 0.9
 
 #initialize population <----- for mosquitos we will have to think carefully about how to do this without 
 #simulating every individual right away. I made this mistake before and crashed a cluster node!
 pop = matrix(nrow=100, ncol=5)            #each individual gets its own row
+colnames(pop) <- c("id", "sex", "mom", "dad") #just to give me a better understanding of what these variables are
 pop[,1] = seq(1,100,1)                    #each individual has unique ID name
 pop[,2] = sample(c(0,1),100,replace=T)    #each individual assigned male (1) or female (0)
 pop[,3] = rep(0,100)                      #mom id - later will not be 0, this is useful for debugging
 pop[,4] = rep(0,100)                      #dad id - later will not be 0, this is useful for debugging
 #...you can add other columsn for other species-specific characteristics you want to track
 
+out1 <- matrix(0, ncol = 2, nrow = 10) # create a matrix for outputting data 
+
 for(r in 1:10){ #this controls the number of times you want to run the simulation - we will run it more than 10 times eventually
   #sample individuals that move from wild to cargo - we will vary this value for sure, so it is a parameter set above
   intransit = pop[sample(seq(1,nrow(pop),1), wildtocargo, replace=F),,drop=F] #randomly select rows from pop that have moved from wild into cargo/ship
   
   #apply some mortality while in transit
-  surviveornot = sample(c(0,1), nrow(intransit), replace=T, prob=c((1-survivetrans), survivetrans))
+  surviveornot = sample(c(0,1), length(intransit), replace=T, prob=c((1-survivetrans), survivetrans))
   intransit    = intransit[which(surviveornot==1), , drop=F] #keep only rows that survived, based on survival probabilty in above line
   
   #some proportion of individuals leave cargo chain and enter wild area at recipient port
-  exitornot = sample(c(0,1), nrow(intransit), replace=T, prob=c(exitcargo, (1-exitcargo)))
+  exitornot = sample(c(0,1), length(intransit), replace=T, prob=c(exitcargo, (1-exitcargo)))
   inwild    = intransit[which(exitornot==1), , drop=F] #keep only rows that exited, based on  probabilty in above line
   intransit = intransit[which(exitornot==0), , drop=F] #update who is in cargo still (this will give some info about repeated port risks)
   
   #some mortality because environment is inhospitable
-  liveornot = sample(c(1,0), nrow(inwild), replace=T, prob=c(newenv, (1-newenv)))
+  liveornot = sample(c(1,0), length(inwild), replace=T, prob=c(newenv, (1-newenv)))
   inwild    = inwild[which(liveornot==1), , drop=F] #keep only rows that exited, based on  probabilty in above line
+  
+  out1[r,1] <- length(inwild)
+  out1[r,2] <- length(intransit)
+  
   
   #reproduction in new location
   
@@ -43,6 +50,8 @@ for(r in 1:10){ #this controls the number of times you want to run the simulatio
   #is possibly interesting down the line, such as the number of mozzies left intransit and how many offspring were "created" in the new location
   
 }
+
+out1
 ###NOTE: the sample function right now is binary, but to the extent possible we will substitute those better distributions. For example, probablitly
 #of surviving in transit is set to 0.9, but if we know the tails of that estimate we can make the model function better.
 
